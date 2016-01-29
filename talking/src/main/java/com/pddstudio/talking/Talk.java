3e8ee856f4dee2065014fd214751228dcd923e5c
Patrick J <patrick.pddstudio@googlemail.com>
2016-01-29 11:26:56 +0100

    Added Copyright

--- talking/src/main/java/com/pddstudio/talking/Talk.java
+++ talking/src/main/java/com/pddstudio/talking/Talk.java
@@ -1,11 +1,23 @@
+/*
+ * Copyright 2016 Patrick J
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
 package com.pddstudio.talking;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
-import android.util.Log;
 
 import com.pddstudio.talking.exceptions.TalkNotInitializedException;
 import com.pddstudio.talking.model.RecognitionModel;
